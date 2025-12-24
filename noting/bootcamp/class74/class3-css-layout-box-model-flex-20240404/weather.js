3dff9a269d9fafe2a2a483473173c6eda71b6c94
xueweili7761-a11y <xueweili7761@gmail.com>
2025-12-23 20:16:37 -0800

    Delete noting directory

--- noting/bootcamp/class74/class3-css-layout-box-model-flex-20240404/weather.js
+++
@@ -1,4 +0,0 @@
-const Ele=document.querySelectorAll('*')
-for(let i=0;i<Ele.length;i++){
-    Ele[i].style.border='1px solid red'
-}
--- noting/bootcamp/class74/mosemove/mosemove.js
+++
@@ -1,8 +0,0 @@
-const ball=document.getElementById('ball')
-
-function move(e){
-    console.log(e.clientX)
-    ball.style.left=e.clientX+'px'
-    ball.style.top=e.clientY+'px'
-}
-document.addEventListener('mousemove',move)
